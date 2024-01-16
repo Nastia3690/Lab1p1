986f53f4ac62db324b378b4080000272a0676d42
Nastia <156798560+Nastia3690@users.noreply.github.com>
2024-01-17 00:04:08 +0300

    Добавьте файлы проекта.

---
+++ Part1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1p1
+{
+    public class Part1
+    {
+        //1) произведение элементов массива с четными номерами
+        public static double product_of_even(ref double[] Arr)
+        {
+            if (Arr.Count() > 1) {
+                double res = 1;
+                for (int i = 1; i < Arr.Count(); i = i + 2)
+                {
+                    res *= Arr[i];
+                }
+                return res;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        // сумму элементов массива, расположенных от begin до end
+        public static double Sum_of_elements(ref double[] Arr,int begin,int end)
+        {
+            double res = 0;
+            for (int i = begin; i <= end; i++)
+            {
+                res += Arr[i];
+            }
+            return res;
+        }
+        // Найдём индекс первого нулевого элемента
+        public static int FindFirstZero(ref double[] Arr)
+        {
+            for (int i = 0; i < Arr.Count(); i++)
+            {
+                if (Arr[i] == 0)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        // Найдём индекс последнего нулевого элемента
+        public static int FindLastZero(ref double[] Arr)
+        {
+            if (Arr.Count() >0)
+            {
+                for (int i = Arr.Count() - 1; i >= 0; i--)
+                {
+                    if (Arr[i] == 0)
+                    {
+                        return i;
+                    }
+                }
+            }
+            return -1;
+        }
+        //Преобразовать массив таким образом, чтобы сначала располагались все положительные элементы,
+        //а потом — все отрицательные(элементы, равные нулю, считать положительными).
+        public static void ChangeArray(ref double[] Arr)
+        {
+            for (int i = 0; i < Arr.Count() - 1; i++)
+            {
+                if (Arr[i] < 0)
+                {
+                    int beg = i + 1;
+                    while (beg < Arr.Count() && Arr[beg] < 0) beg++;
+                    if (beg < Arr.Count() && Arr[beg] > 0)
+                    {
+                        double tmp = Arr[i];
+                        Arr[i] = Arr[beg];
+                        Arr[beg] = tmp;
+                    }
+                }
+            }
+        }
+        //Ввод массива
+        public static bool InputArray(ref double[] Arr)
+        {
+            try
+            {
+                Console.WriteLine("Введите элементы массива: ");
+                for (int i = 0; i < Arr.Count(); i++)
+                {
+                    Arr[i] = double.Parse(Console.ReadLine());
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        //Вывод массива
+        public static void DisplayArray(ref double[] Arr)
+        {
+            Console.Write("Массив: ");
+            foreach (double item in Arr)
+            {
+                Console.Write(item.ToString()+" ");
+            }
+            Console.Write("\n");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1p1
+{
+    public class Program
+    {
+        //В одномерном массиве, состоящем из N целочисленных элементов, вычислить:
+        //1) произведение элементов массива с четными номерами
+        //2) сумму элементов массива, расположенных между первым и последним нулевыми элементами.
+        //Преобразовать массив таким образом, чтобы сначала располагались все положительные элементы,
+        //а потом — все отрицательные(элементы, равные нулю, считать положительными).
+        static void Main(string[] args)
+        {
+            double[] Mas;
+            Console.WriteLine("Введите число элементов массива: ");
+            int N = int.Parse(Console.ReadLine());
+            if (N > 0)
+            {
+                Mas = new double[N];
+                Part1.InputArray(ref Mas);
+                Console.WriteLine($"Произведение элементов массива с четными номерами: {Part1.product_of_even(ref Mas)}");
+                int first = Part1.FindFirstZero(ref Mas);
+                int last = Part1.FindLastZero(ref Mas);
+                if (first==-1)
+                {
+                    Console.WriteLine("В массиве нет нулевых элементов!");
+                }
+                else
+                if (last == first)
+                {
+                    Console.WriteLine("В массиве один нулевой элемент!");
+                }
+                else
+                if (last == first + 1)
+                {
+                    Console.WriteLine("В массиве нет элементов между нулевыми элементами!");
+                }
+                else
+                {
+                    Console.WriteLine($"Сумма элементов, расположенных между первым и последним нулевыми элементами: {Part1.Sum_of_elements(ref Mas,first+1,last-1)}");
+                }
+                Part1.ChangeArray(ref Mas);
+                Part1.DisplayArray(ref Mas);
+            }
+            else
+            {
+                Console.WriteLine("Ошибка ввода числа элементов массива!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
